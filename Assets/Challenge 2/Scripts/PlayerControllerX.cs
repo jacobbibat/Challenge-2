367c13042e934b72a7ac00889bcd2ce6785e2ff8
jacobbibat <b00165639@mytudublin.ie>
2024-10-06 15:20:29 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float spawnCooldown = 1.6f;
+    private float spawnTime = 0.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTime += Time.deltaTime; // When spacebar is pressed and the time between the last spawn is >= cooldown time, Input can be executed
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && spawnTime >= spawnCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            spawnTime = 0.0f; // Reset's timer to 0 seconds
+        }
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnRange = 18.0f;
+    private float spawnPosY = 35;
+
+    private float startDelay = 3.0f;
+    private float spawnInterval = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float randomRange = Random.Range(startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomBall", startDelay, randomRange);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRange, spawnRange), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+}
